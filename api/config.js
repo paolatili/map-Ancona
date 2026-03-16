1e329e9939282effc1e4c108e3773944e61afcf8
paolatili <paolatili01@gmail.com>
2026-03-16 19:45:26 +0100

    config

--- api/config.js
+++ api/config.js
@@ -1,6 +1,5 @@
 export default function handler(req, res) {
   const key = process.env.GMAPS_API_KEY;
 
-  res.setHeader("Content-Type", "application/javascript");
   res.status(200).send(`window.GMAPS_API_KEY="${key}";`);
 }
